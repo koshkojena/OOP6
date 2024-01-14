14de9903e98789fc41954a14cb459f5db6df6f9b
koshkojena <156100162+koshkojena@users.noreply.github.com>
2024-01-14 18:19:33 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace лабораторная_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 403);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Items.AddRange(new object[] {
+            "110010101000",
+            "101010101001001001010001111",
+            "1110",
+            "0001",
+            "10101",
+            "101010101010Ф",
+            "10100101",
+            "01100101",
+            "101010001",
+            "10100",
+            "11",
+            "1",
+            "0",
+            "10"});
+            this.listBox2.Location = new System.Drawing.Point(17, 13);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(388, 356);
+            this.listBox2.TabIndex = 3;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(191, 408);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace лабораторная_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Form1_Load(null, null);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Text = "Пуск";
+            button1.Click += button1_Click;
+            label1.Text = "Выберите строку и нажмите пуск";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            // Проверяем, выбрана ли строка
+            if (listBox2.SelectedIndex == -1)
+            {
+                MessageBox.Show("Ошибка: строка не выбрана!");
+                return;
+            }
+            // Получаем номер выделенной строки
+            int index = listBox2.SelectedIndex;
+            // Считываем строку в переменную str
+            string str = (string)listBox2.Items[index];
+            int zeroCount = 0; // Счетчик нулей
+            int oneCount = 0; // Счетчик единиц
+            // Узнаем количество символов в строке
+            int len = str.Length;
+            // Перебор всех символов строки и подсчет количества нулей и единиц
+            foreach (char c in str)
+            {
+                if (c == '0')
+                {
+                    zeroCount++;
+                }
+                else if (c == '1')
+                {
+                    oneCount++;
+                }
+            }
+            label1.Text = "Количество единиц = " + oneCount.ToString() + ". Количество нулей =  " + zeroCount.ToString();//вывод результата
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
